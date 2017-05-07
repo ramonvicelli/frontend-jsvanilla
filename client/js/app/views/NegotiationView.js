208421eb112ba0abcf5bfd78faaa731f6950fc99
Ramon <ramonvicelli@gmail.com>
2017-05-07 10:30:53 +0100

    create render table

--- client/js/app/views/NegotiationView.js
+++ client/js/app/views/NegotiationView.js
@@ -1,25 +1,30 @@
 class NegotiationView {
 
 
-  constructor(){
-    this._idTable = '#negotiations';
-
+  constructor() {
+    const ID_TABLE = '#negotiations';
+    this.table = document.querySelector(ID_TABLE);
   }
 
   update(negotiation) {
-    document.querySelector(`${this._idTable} tbody`)
-      .innerHTML += this._newRow(negotiation);
+    this.table.tBodies[0].innerHTML += this._newBodyRow(negotiation);
+    this._updateFoot(negotiation);
   }
 
-  _newRow(negotiation){
-    return `
-      <tr>
+  _newBodyRow(negotiation) {
+    return `<tr>
         <td>${DateHelper.dateToString(negotiation.date)}</td>
         <td>${negotiation.amount}</td>
         <td>${negotiation.value}</td>
         <td>${negotiation.volume}</td>
-      </tr>
-    `;
+      </tr>`;
   }
 
+  _updateFoot(negotiation) {
+    const tr = this.table.tFoot.firstElementChild;
+    console.log(tr);
+    const tdValue = tr.lastElementChild;
+    console.log(tdValue);
+    tdValue.innerHTML = Number(tdValue.innerHTML) + negotiation.volume;
+  }
 }
