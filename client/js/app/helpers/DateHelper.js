ee97ee5c17889fd5cdf8c80239275ae520220b5f
Ramon <ramonvicelli@gmail.com>
2017-05-07 09:02:08 +0100

    create DateHelper

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,19 @@
+class DateHelper {
+
+  constructor() {
+    throw new Error("DateHelper can't be instantiated")
+  }
+
+  static stringToDate(text) {
+    if (!/\d{4}-\d{2}-\d{2}/.test(text)) {
+      throw new Error('Should be in format yyyy-mm-dd');
+    }
+
+    return new Date(text.split('-'));
+    // return new Date(text.replace(/-/g,','));
+  }
+
+  static dateToString(date) {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+  }
+}
