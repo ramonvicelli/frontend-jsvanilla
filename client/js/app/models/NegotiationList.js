ad7e6a7da98c6c8b80e373e32a03ce0b9d4172ff
Ramon <ramonvicelli@gmail.com>
2017-05-07 09:06:45 +0100

    create model NegotiationList

---
+++ client/js/app/models/NegotiationList.js
@@ -0,0 +1,14 @@
+class NegotiationList {
+
+  constructor() {
+    this._negotiations = [];
+  }
+
+  add(negotiation) {
+    this._negotiations.push(negotiation);
+  }
+
+  get negotiations() {
+    this._negotiations;
+  }
+}
