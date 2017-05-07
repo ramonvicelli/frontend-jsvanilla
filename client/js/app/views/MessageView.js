6331d81d38a1716b6a39987aa563839dfef0fc79
Ramon <ramonvicelli@gmail.com>
2017-05-07 14:20:22 +0100

    fix acess property text of message

--- client/js/app/views/MessageView.js
+++ client/js/app/views/MessageView.js
@@ -4,10 +4,11 @@ class MessageView {
   }
 
   _template(model) {
-    return `<p class="alert alert-info">${model}</p>`;
+    return `<p class="alert alert-info">${model.text}</p>`;
   }
 
   update(model) {
+    console.log(this._element);
     this._element.innerHTML = this._template(model);
   }
 }
