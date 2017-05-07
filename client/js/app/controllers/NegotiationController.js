81810bcbd1f582ae92a6e49d0c02e480bb9ee998
Ramon <ramonvicelli@gmail.com>
2017-05-07 09:12:40 +0100

    refactoring negotiationController, create method createNegotiation

--- client/js/app/controllers/NegotiationController.js
+++ client/js/app/controllers/NegotiationController.js
@@ -13,21 +13,23 @@ class NegotiationController {
   add(event) {
     event.preventDefault();
 
-    const negotiation = new Negotiation(
-      DateHelper.stringToDate(this._date.value),
-      this._amount.value,
-      this._value.value
-    );
-
-    this._negotiationsList.add(negotiation);
+    this._negotiationsList.add(this._createNegotiation());
     this._cleanForm();
   }
 
-  _cleanForm(){
+  _cleanForm() {
     this._amount.value = 1;
     this._date.value = '';
     this._value.value = 0.0
 
     this._amount.focus();
+  }
+
+  _createNegotiation() {
+    return new Negotiation(
+      DateHelper.stringToDate(this._date.value),
+      this._amount.value,
+      this._value.value
+    );
   }
 }
