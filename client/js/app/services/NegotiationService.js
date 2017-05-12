aca6423d31a31968724d1c795265995c3d627902
Ramon <ramonvicelli@gmail.com>
2017-05-12 21:18:16 +0100

    Refactor NegotiationService, created HttpService

--- client/js/app/services/NegotiationService.js
+++ client/js/app/services/NegotiationService.js
@@ -1,65 +1,23 @@
 class NegotiationService {
-
-  getByWeek(cb) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-
-      xhr.open('GET', 'negotiation/week');
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText)
-              .map(item => new Negotiation(new Date(item.date), item.amount, item.value))
-            );
-          } else {
-            console.log("Could not retrieve the negotiatios =>", xhr.responseText);
-            reject('Could not retrieve the negotiatios of the week.');
-          }
-        }
-      }
-      xhr.send();
-    });
+  constructor() {
+    this._httpService = new HttpService();
   }
 
-  getByLastWeek(cb) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-
-      xhr.open('GET', 'negotiation/lastweek');
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText)
-              .map(item => new Negotiation(new Date(item.date), item.amount, item.value))
-            );
-          } else {
-            console.log("Could not retrieve the negotiatios =>", xhr.responseText);
-            reject('Could not retrieve the negotiatios of the last week.');
-          }
-        }
-      }
-      xhr.send();
-    });
+  getByWeek() {
+    return this._httpService.get('negotiation/week')
+      .then(itens => itens.map(item => new Negotiation(new Date(item.date), item.amount, item.value)))
+      .catch(error => 'Could not retrieve the negotiatios of the week.');
   }
 
-  getByDelayedWeek(cb) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
+  getByLastWeek() {
+    return this._httpService.get('negotiation/week')
+      .then(itens => itens.map(item => new Negotiation(new Date(item.date), item.amount, item.value)))
+      .catch(error => 'Could not retrieve the negotiatios of the last week.');
+  }
 
-      xhr.open('GET', 'negotiation/delayedweek');
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText)
-              .map(item => new Negotiation(new Date(item.date), item.amount, item.value))
-            );
-          } else {
-            console.log("Could not retrieve the negotiatios =>", xhr.responseText);
-            reject('Could not retrieve the negotiations of the delayed week.');
-          }
-        }
-      }
-      xhr.send();
-    });
+  getByDelayedWeek() {
+    return this._httpService.get('negotiation/delayedweek')
+      .then(itens => itens.map(item => new Negotiation(new Date(item.date), item.amount, item.value)))
+      .catch(error => 'Could not retrieve the negotiatios of the delayed week.');
   }
 }
