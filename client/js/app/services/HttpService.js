af143c96c1bde93ab9c1d2a0e270f995f913ed72
Ramon <ramonvicelli@gmail.com>
2017-05-13 21:44:44 +0100

    Using FetchAPI

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -1,20 +1,16 @@
 class HttpService {
-  get(url) {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
 
-      xhr.open('GET', url);
-      xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4) {
-          if (xhr.status === 200) {
-            resolve(JSON.parse(xhr.responseText));
-          } else {
-            console.log(xhr.responseText);
-            reject(xhr.responseText);
-          }
-        }
-      }
-      xhr.send();
-    });
+  get(url) {
+    return fetch(url)
+      .then(res => this._handleErrors(res))
+      .then(res => res.json());
+  }
+
+  _handleErrors(res) {
+    if (res.ok) {
+      return res;
+    }
+
+    throw new Error(res.statusText);
   }
 }
