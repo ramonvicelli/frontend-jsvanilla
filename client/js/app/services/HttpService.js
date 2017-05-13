9cdd67e16156b04fa88b074930b5febaf613b1f8
Ramon <ramonvicelli@gmail.com>
2017-05-13 22:03:07 +0100

    polyfill fetchapi

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -6,6 +6,15 @@ class HttpService {
       .then(res => res.json());
   }
 
+  post(url, data) {
+    return fetch(url, {
+        headers: {'Content-type': 'application/json'},
+        method: 'post',
+        body: JSON.stringify(data)
+      })
+      .then(res => this._handleErrors(res));
+  }
+
   _handleErrors(res) {
     if (res.ok) {
       return res;
