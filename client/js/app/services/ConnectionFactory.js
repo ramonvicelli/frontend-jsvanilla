41eaa82a4bf5ac15edb59dcc39a6b4b295f6a52f
Ramon <ramonvicelli@gmail.com>
2017-05-13 19:11:03 +0100

    Create ConnnectionFactory, using IndexedDB

---
+++ client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,60 @@
+const ConnectionFactory = (() => {
+  const dbName = 'frontend-jsvanilla';
+  const version = 1;
+  const stores = ['negotiation'];
+
+  let close = null;
+  let connection = null;
+
+  return class ConnectionFactory {
+
+    constructor() {
+      throw new Error("ConnectionFactory can't be instantiated")
+    }
+
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+
+        const openRequest = window.indexedDB.open(dbName, version);
+
+        openRequest.onupgradeneeded = e => this._createStore(e.target.result);
+
+        openRequest.onsuccess = e => {
+          if (!connection) {
+            connection = e.target.result;
+
+            close = connection.close.bind(connection);
+            connection.close = function () {
+              throw new Error("Close can't be called");
+            };
+          }
+          resolve(connection);
+        }
+
+        openRequest.onerror = e => {
+          console.log(e.target.error);
+          reject(e.target.error.name);
+        };
+      });
+    }
+
+    static closeConnection() {
+      if (connection) {
+        close();
+        connection = null;
+      }
+    }
+
+    static _createStore(connection) {
+      stores.forEach(store => {
+        if (connection.objectStoreNames.contains(store)) {
+          connection.deleteObjectStore(store);
+        }
+
+        connection.createObjectStore(store, {
+          autorIncrement: true
+        });
+      });
+    }
+  }
+})();
