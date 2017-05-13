5d51d45db394456ce9fed3f91bae8c858ec1dafb
Ramon <ramonvicelli@gmail.com>
2017-05-13 20:28:40 +0100

    NegotiationService add method get only

--- client/js/app/services/NegotiationService.js
+++ client/js/app/services/NegotiationService.js
@@ -3,19 +3,23 @@ class NegotiationService {
     this._httpService = new HttpService();
   }
 
-  getByWeek() {
+  get(){
+    return Promise.all([this._getByWeek(), this._getByLastWeek(), this._getByDelayedWeek()]);
+  }
+
+  _getByWeek() {
     return this._httpService.get('negotiation/week')
       .then(itens => itens.map(item => new Negotiation(new Date(item.date), item.amount, item.value)))
       .catch(error => 'Could not retrieve the negotiatios of the week.');
   }
 
-  getByLastWeek() {
+  _getByLastWeek() {
     return this._httpService.get('negotiation/week')
       .then(itens => itens.map(item => new Negotiation(new Date(item.date), item.amount, item.value)))
       .catch(error => 'Could not retrieve the negotiatios of the last week.');
   }
 
-  getByDelayedWeek() {
+  _getByDelayedWeek() {
     return this._httpService.get('negotiation/delayedweek')
       .then(itens => itens.map(item => new Negotiation(new Date(item.date), item.amount, item.value)))
       .catch(error => 'Could not retrieve the negotiatios of the delayed week.');
